5a3f3d7d2eb4d3baa9bd331b8b03595e8c758fd6
patriotpie <patriotpie@gmail.com>
2014-02-15 20:16:59 -0500

    Initial commit: basic scene and assets

---
+++ Assets/Scripts/NavmeshSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavmeshSpawner : MonoBehaviour {
+	
+	public GameObject point;
+	
+	public float width;
+	public float height;
+	public float step;
+	
+	// Use this for initialization
+	void Start () {
+		
+		for (float x = -width/2f; x < width/2f; x += step) {
+			for (float y = -height/2f; y < height/2f; y += step) {
+				Instantiate(point, new Vector3(x, y, 1), Quaternion.identity);
+			}
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
